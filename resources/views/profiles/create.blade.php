1967fc5477bbcf4c12ef54fab7c86dcf0fd9ecd4
jtreeves <jackson.t.reeves@gmail.com>
2022-03-26 18:44:06 -0400

    [views] Add form to create a new profile

---
+++ resources/views/profiles/create.blade.php
@@ -0,0 +1,84 @@
+@extends('layouts.main', ['title' => $title])
+
+@section('content')
+    <form
+        action="{{ route('profiles.store') }}"
+        method="POST"
+    >
+        <article>
+            <label 
+                for="username"
+            >
+                Username
+            </label>
+    
+            <input 
+                type="text"
+                name="username"
+                id="username"
+            />
+        </article>
+        
+        <article>
+            <label 
+                for="avatar"
+            >
+                Avatar
+            </label>
+    
+            <input 
+                type="text"
+                name="avatar"
+                id="avatar"
+            />
+        </article>
+        
+        <article>
+            <label 
+                for="bio"
+            >
+                Bio
+            </label>
+    
+            <input 
+                type="text"
+                name="bio"
+                id="bio"
+            />
+        </article>
+        
+        <article>
+            <label 
+                for="location"
+            >
+                Location
+            </label>
+    
+            <input 
+                type="text"
+                name="location"
+                id="location"
+            />
+        </article>
+        
+        <article>
+            <label 
+                for="movie"
+            >
+                Movie
+            </label>
+    
+            <input 
+                type="text"
+                name="movie"
+                id="movie"
+            />
+        </article>
+
+        <button
+            type="submit"
+        >
+            Save Your Profile
+        </button>
+    </form>
+@endsection
