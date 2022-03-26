9fa02f3fcba53ded11b52f556d216730f640968f
jtreeves <jackson.t.reeves@gmail.com>
2022-03-26 16:56:14 -0400

    [views] Create about page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+@php
+    $title = 'About';
+@endphp
+
+@extends('layouts.main', ['title' => $title])
+
+@section('content')
+    <p>
+        You can create a profile, and then create and save Star Wars mashups.
+    </p>
+@endsection
