d0df1be0778727d1bf23164b85be8b65c79bbf6f
jtreeves <jackson.t.reeves@gmail.com>
2022-03-26 16:56:28 -0400

    [views] Create home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@php
+    $title = 'Home';
+@endphp
+
+@extends('layouts.main', ['title' => $title])
+
+@section('content')
+    <p>
+        Create and save Star Wars mashups!
+    </p>
+@endsection
