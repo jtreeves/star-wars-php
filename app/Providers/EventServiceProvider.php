9f0f3347c46e835a48c923da5225db845aa96bb4
jtreeves <jackson.t.reeves@gmail.com>
2022-03-28 21:29:22 -0400

    [EventServiceProvider] Register new events and their listeners

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Events\StarMashup;
+use App\Events\UnstarMashup;
+use App\Listeners\AddFavorite;
+use App\Listeners\RemoveFavorite;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -17,6 +21,12 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+        StarMashup::class => [
+            AddFavorite::class,
+        ],
+        UnstarMashup::class => [
+            RemoveFavorite::class,
         ],
     ];
 
