19f91b2b78625f0b0113407f8266aa436a89e9a3
jtreeves <jackson.t.reeves@gmail.com>
2022-03-28 21:25:02 -0400

    [database] Replace favorites with more traditional mashup_profile pivot table to fix glitches

--- database/migrations/2022_03_23_024944_create_profiles_table.php
+++ database/migrations/2022_03_23_024944_create_profiles_table.php
@@ -24,7 +24,7 @@ return new class extends Migration
             $table->string('location');
             $table->string('avatar');
             $table->string('movie');
-            
+
             $table->timestamps();
         });
     }
