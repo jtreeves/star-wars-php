c1ec8c0dfaf3f13013004f4de06eb0df865eb7ff
jtreeves <jackson.t.reeves@gmail.com>
2022-03-29 21:16:32 -0400

    [views/mashups] Add unfavorite option for button; add list of all profiles that have liked the mashup

--- resources/views/mashups/show.blade.php
+++ resources/views/mashups/show.blade.php
@@ -17,21 +17,63 @@
             }}"
         />
 
-        <form 
-            action="{{ route(
-                'favorites.star', 
-                $mashup->id
-            ) }}"
-            method="GET"
-        >
-            @csrf
-
-            <button
-                type="submit"
+        @if (!$mashup->profiles->contains(
+            'id', 
+            Auth::user()->profile->id
+        ))
+            <form 
+                action="{{ route(
+                    'favorites.star', 
+                    $mashup->id
+                ) }}"
+                method="GET"
             >
-                Favorite
-            </button>
-        </form>
+                @csrf
+    
+                <button
+                    type="submit"
+                >
+                    Favorite
+                </button>
+            </form>
+        @else
+            <form 
+                action="{{ route(
+                    'favorites.unstar', 
+                    $mashup->id
+                ) }}"
+                method="GET"
+            >
+                @csrf
+    
+                <button
+                    type="submit"
+                >
+                    Unfavorite
+                </button>
+            </form>
+        @endif
+
+        @if (count($mashup->profiles) != 0)
+            <ul>
+                @foreach ($mashup->profiles as $profile)
+                    <li>
+                        <a href="{{ route(
+                            'profiles.show',
+                            $profile->id
+                        ) }}">
+                            <p>
+                                {{ $profile->username }}
+                            </p>
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+        @else
+            <p>
+                This mashup has not been favorited by any users.
+            </p>
+        @endif
     </article>
 
     <form 
