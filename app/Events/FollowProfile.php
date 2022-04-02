c29491b37747c1c72c3f79dc39b2878ba502531e
jtreeves <jackson.t.reeves@gmail.com>
2022-04-02 19:26:54 -0400

    [Events] Create events for following and unfollowing other users

---
+++ app/Events/FollowProfile.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Profile;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class FollowProfile implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    // The profile used by the event
+    public Profile $profile;
+
+    // Create a new event instance
+    public function __construct(Profile $profile)
+    {
+        $this->profile = $profile;
+    }
+
+    // Get the channels the event should broadcast on
+    public function broadcastOn(): Channel
+    {
+        return new PrivateChannel('following');
+    }
+}
