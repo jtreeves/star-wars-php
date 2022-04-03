6d573df266bd5fe37bb7c868c2c70ba2c2db2143
jtreeves <jackson.t.reeves@gmail.com>
2022-04-02 20:03:40 -0400

    [views] Add lists of followers and followings to show page for profiles

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -51,6 +51,56 @@
         message="This user has not liked any mashups."
     />
 
+    <article>
+        <h2>
+            Followers
+        </h2>
+
+        @if (count($profile->followers) != 0)
+            <ul>
+                @foreach ($profile->followers as $follower)
+                    <li>
+                        <a href="{{ route(
+                            'profiles.show',
+                            $follower->id
+                        ) }}">
+                            {{ $follower->username }}
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+        @else
+            <p>
+                This user does not have any followers.
+            </p>
+        @endif
+    </article>
+    
+    <article>
+        <h2>
+            Following
+        </h2>
+
+        @if (count($profile->followings) != 0)
+            <ul>
+                @foreach ($profile->followings as $following)
+                    <li>
+                        <a href="{{ route(
+                            'profiles.show',
+                            $following->id
+                        ) }}">
+                            {{ $following->username }}
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+        @else
+            <p>
+                This user does not follow any other users.
+            </p>
+        @endif
+    </article>
+
     @if (Auth::user()->profile->id == $profile->id)
         <form 
             action="{{ route(
