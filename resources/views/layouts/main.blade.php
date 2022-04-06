d998e6ce27b9db09c830fc17bdde7b1ab13e472f
jtreeves <jackson.t.reeves@gmail.com>
2022-04-05 23:02:54 -0400

    [views] Set background color based on mode and logged in user's color choice

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html 
     lang="{{ str_replace('_', '-', app()->getLocale()) }}"
+    class="{{ session('mode') }}"
 >
     <head>
         <base 
@@ -41,8 +42,50 @@
         ></script>
     </head>
 
+    @php
+        if (!Auth::check()) {
+            $backgroundColor = 'bg-gray-100 dark:bg-gray-700';
+        } else {
+            switch (Auth::user()->profile->color) {
+                case 'red':
+                    $backgroundColor = 'bg-red-300 dark:bg-red-700';
+                    break;
+                case 'orange':
+                    $backgroundColor = 'bg-orange-300 dark:bg-orange-700';
+                    break;
+                case 'yellow':
+                    $backgroundColor = 'bg-yellow-300 dark:bg-yellow-700';
+                    break;
+                case 'green':
+                    $backgroundColor = 'bg-green-300 dark:bg-green-700';
+                    break;
+                case 'blue':
+                    $backgroundColor = 'bg-blue-300 dark:bg-blue-700';
+                    break;
+                case 'purple':
+                    $backgroundColor = 'bg-purple-300 dark:bg-purple-700';
+                    break;
+                case 'pink':
+                    $backgroundColor = 'bg-pink-300 dark:bg-pink-700';
+                    break;
+                case 'amber':
+                    $backgroundColor = 'bg-amber-300 dark:bg-amber-700';
+                    break;
+                case 'teal':
+                    $backgroundColor = 'bg-teal-300 dark:bg-teal-700';
+                    break;
+                case 'zinc':
+                    $backgroundColor = 'bg-zinc-300 dark:bg-zinc-700';
+                    break;
+                default:
+                    $backgroundColor = 'bg-gray-100 dark:bg-gray-700';
+                    break;
+            }
+        }
+    @endphp
+
     <body 
-        class="font-cycle antialiased min-h-screen bg-gray-100 grid grid-cols-1 grid-rows-[100px_auto_100px] justify-items-center"
+        class="font-cycle antialiased min-h-screen grid grid-cols-1 grid-rows-[100px_auto_100px] justify-items-center {{ $backgroundColor }}"
     >
         @include('partials.header')
 
