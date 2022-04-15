caa911c815a881b8d25f6bb935ddf275f6c26a95
jtreeves <jackson.t.reeves@gmail.com>
2022-04-15 17:52:00 -0400

    [toggle] Style as slider

--- resources/views/partials/toggle.blade.php
+++ resources/views/partials/toggle.blade.php
@@ -1,33 +1,27 @@
-@php
-    $isLight = session('mode') == 'light';
-    $active = 'bg-blue-700';
-    $inactive = 'bg-blue-300';
-    $lightClass = $isLight ? $active : $inactive;
-    $darkClass = !$isLight ? $active : $inactive;
-@endphp
-
-<article class="flex justify-center gap-3">
+<article class="flex justify-center gap-3 border-solid border-4 border-slate-500 h-fit rounded-full bg-slate-300 dark:bg-slate-700">
     <form 
         action="{{ route('mode.light') }}"
         method="GET"
+        class="w-6 h-6"
     >
         @csrf
 
         <button
             type="submit"
-            class="{{ $lightClass }} rounded-full w-6 h-6"
+            class="bg-slate-100 ring-2 ring-slate-900 dark:bg-transparent dark:ring-0 rounded-full w-6 h-6"
         ></button>
     </form>
     
     <form 
         action="{{ route('mode.dark') }}"
         method="GET"
+        class="w-6 h-6"
     >
         @csrf
 
         <button
             type="submit"
-            class="{{ $darkClass }} rounded-full w-6 h-6"
+            class="bg-transparent ring-0 dark:bg-slate-900 dark:ring-2 dark:ring-slate-100 rounded-full w-6 h-6"
         ></button>
     </form>
 </article>
